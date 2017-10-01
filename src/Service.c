b786858b12fbf4cdcd186ad407eae9f12ea60acd
Sander Mertens <sander.mertens8@gmail.com>
2017-10-01 18:55:36 +0000

    Enable automatic redirects for router

--- src/Service.c
+++ src/Service.c
@@ -1,6 +1,7 @@
 /* This is a managed file. Do not delete this comment. */
 
 #include <corto/httprouter/httprouter.h>
+
 int16_t httprouter_Service_onRequest(
     httprouter_Service this,
     httpserver_HTTP_Connection c,
@@ -11,8 +12,14 @@ int16_t httprouter_Service_onRequest(
     corto_any result = {corto_type(corto_string_o), &resultStr, FALSE};
     corto_any param = {corto_type(httpserver_HTTP_Request_o), r, FALSE};
     corto_route route = NULL;
+    corto_id idbuff;
+
+    corto_debug("httprouter: match uri '%s' to routes of '%s'",
+        uri,
+        corto_fullpath(NULL, this));
+    
     if (corto_router_match(this, uri, param, result, &route)) {
-        corto_debug("web: request '%s' not matched to routes in '%s' of type '%s'", 
+        corto_debug("httprouter: request '%s' not matched to routes in '%s' of type '%s'", 
             r->uri, 
             corto_fullpath(NULL, this),
             corto_fullpath(NULL, corto_typeof(this)));
@@ -30,5 +37,12 @@ int16_t httprouter_Service_onRequest(
     return 1;
 nomatch:
     return 0;
+}
+
+int16_t httprouter_Service_construct(
+    httprouter_Service this)
+{
+    httpserver_Service(this)->redirectEndpointToPath = true;
+    return corto_super_construct(this);
 }
 
